da1a801233819c1c617f88f7df1da5686c573d6a
shalini <shalini.v@tvsnext.io>
2021-07-08 19:52:48 +0530

    [shalini]FindMaxInteger

---
+++ FindMaxValue/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+namespace FindMaxValue
+{
+    
+
+        
+        public class Program
+        {
+            public int FindMaximum(int first, int second, int third)
+            {
+                if (first.CompareTo(second) > 0 && first.CompareTo(third) > 0)
+                {
+                    return first;
+                }
+                else if (second.CompareTo(first) > 0 && second.CompareTo(third) > 0)
+                {
+                    return second;
+                }
+                else if (third.CompareTo(second) > 0 && third.CompareTo(first) > 0)
+                {
+                    return third;
+                }
+                else
+                {
+                    Console.WriteLine("All three having same number");
+                    return -1;
+                }
+            }
+
+
+
+            static void Main(string[] args)
+            {
+            Program obj = new Program();
+            int maxValue=obj.FindMaximum(70, 35, 56);
+            Console.WriteLine(maxValue);
+            Console.ReadLine();
+            }
+        
+            
+        }
+    
+}
